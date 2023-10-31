0b3a1f93ad6dd0242e2b4478be45a8706c026b16
misung.dev <misung.dev@gmail.com>
2023-11-01 01:50:25 +0900

    [Feat]: 헤더 생성

--- 윈터/5주차 미션/src/App.js
+++ 윈터/5주차 미션/src/App.js
@@ -1,44 +1,46 @@
-import React from "react";
-import "./index.css";
-import { MovieCard } from "./Components";
-import { movies, TMDB_IMAGE_BASE_URL } from "./database";
-
-import styled from "@emotion/styled";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./Components/Header";
+import Celebrity from "./pages/Celebrity";
+import Home from "./pages/Home";
+import Movies from "./pages/Movies";
+import NotFound from "./pages/NotFound";
+import TV from "./pages/TV";
+// import styled from "@emotion/styled";
 
 function App() {
 	return (
-		<Container>
-			<MovieCardList>
-				{movies.results.map(({ id, title, overview, poster_path, vote_average }) => (
-					<MovieCardItem key={id}>
-						<MovieCard
-							title={title}
-							overview={overview}
-							posterPath={`${TMDB_IMAGE_BASE_URL}${poster_path}`}
-							voteAverage={vote_average}
-						/>
-					</MovieCardItem>
-				))}
-			</MovieCardList>
-		</Container>
+		<div className="root-wrap ">
+			<div className="root-wrap">
+				<BrowserRouter>
+					<Header />
+					<Routes>
+						<Route path="/" element={<Home />} />
+						<Route path="/movies" element={<Movies />} />
+						<Route path="/tv" element={<TV />} />
+						<Route path="/celebrity" element={<Celebrity />} />
+						<Route path="*" element={<NotFound />} />
+					</Routes>
+				</BrowserRouter>
+			</div>
+		</div>
 	);
 }
 
-const Container = styled.div`
-	background-color: #22264c;
-	padding: 20px 0px;
-`;
+// const Container = styled.div`
+// 	background-color: #22264c;
+// 	padding: 20px 0px;
+// `;
 
-const MovieCardList = styled.ul`
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	gap: 20px;
-	flex-wrap: wrap;
-`;
+// const MovieCardList = styled.ul`
+// 	display: flex;
+// 	justify-content: center;
+// 	align-items: center;
+// 	gap: 20px;
+// 	flex-wrap: wrap;
+// `;
 
-const MovieCardItem = styled.li`
-	max-width: 320px;
-`;
+// const MovieCardItem = styled.li`
+// 	max-width: 320px;
+// `;
 
 export default App;
