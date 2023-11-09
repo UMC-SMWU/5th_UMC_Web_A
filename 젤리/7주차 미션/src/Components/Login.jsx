aae9019770b18a65921a8e5b90f126ae2af22482
hanjeonghyun <limithyunii@gmail.com>
2023-11-09 13:19:48 +0900

    [Feat(젤리/한정현)] 7주차 TV프로그램 구현 미션

---
+++ 젤리/7주차 미션/src/Components/Login.jsx
@@ -0,0 +1,30 @@
+// import React from 'react'
+// import { useNavigate } from 'react-router-dom'
+// import styled from 'styled-components';
+
+// const Loginbox = styled.div`
+//     background-color: white;
+//     display: center;
+// `;
+
+// const Logemail = styled.p`
+    
+// `;
+
+// export default function Login() {
+//     const navigate = useNavigate();
+//     return (
+//         <div className='page-container'>
+//             <div className='Loginbox'>
+//                 <h2>이메일과 비밀번호를 입력해주세요</h2>
+//                 <form action="submit">
+//                     <p>이메일 주소</p>
+//                     <input type="email" />
+//                     <br />
+//                     <p>비밀번호</p>
+//                     <input type="password" placeholder='영문, 숫자, 특수문자 포함 8자 이상' />
+//                 </form>
+//             </div>
+//         </div>
+//     )
+// }
---
+++ 젤리/7주차 미션/src/Components/LoginControl.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
+import styled from 'styled-components';
+
+const Button = styled.button`
+    background-color: white;
+    text-color: rgba(3, 37, 65, 1);
+    padding: 10px 20px;
+    border-radius: 2em;
+    border-line: none;
+    cursor: pointer;
+    margin: 10px;
+`;
+
+const P = styled.p`
+    color: white;    
+`;
+
+export default function LoginControl() {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+    const navigate = useNavigate();
+
+    const handleLoginClick = () => {
+        setIsLoggedIn(true);
+        // navigate(`/Login`);
+    }
+    const handleLogoutClick = () => {
+        setIsLoggedIn(false);
+    }
+
+    return (
+        <div>
+            {isLoggedIn ? (
+                <P><Button onClick={handleLogoutClick}>로그아웃</Button>환영합니다!</P>
+            ) : (
+                <P><Button onClick={handleLoginClick}>로그인</Button>로그인 해주세요! </P>
+            )}
+        </div>
+    );
+}
