9dcebaa6baccf039f9c8dcfbbc734dc9a48b55bd
misung.dev <misung.dev@gmail.com>
2023-11-14 19:28:16 +0900

    [Feat(윈터/류미성)]: 정규식표현을 활용한 input창 입력 형식 제한 및 하단 버튼 disabled 속성 변경

--- 윈터/7주차 미션/src/Components/Login.jsx
+++ 윈터/7주차 미션/src/Components/Login.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "@emotion/styled";
 
 const Container = styled.div`
@@ -90,13 +90,48 @@ const Button = styled.button`
 	cursor: pointer;
 
 	// 버튼이 활성화되지 않았을때의 상태를 지정해줌
-	&::disabled {
+	&:disabled {
 		background-color: #dadada;
 		color: #fff;
 	}
 `;
 
 export default function Login() {
+	const [email, setEmail] = useState(""); //빈 문자열로 초기화
+	const [pw, setPw] = useState("");
+
+	const [emailValid, setEmailValid] = useState(false);
+	const [pwValid, setPwValid] = useState(false);
+	const [notAllow, setNotAllow] = useState(true); // 확인버튼의 활성화와 비활성화
+
+	const handleEmail = (e) => {
+		setEmail(e.target.value);
+		const regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/; // 이메일 정규식
+		if (regex.test(email)) {
+			setEmailValid(true);
+		} else {
+			setEmailValid(false);
+		}
+	};
+
+	const handlePassword = (e) => {
+		setPw(e.target.value);
+		const regex = /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/; // 비밀번호 정규식
+		if (regex.test(pw)) {
+			setPwValid(true);
+		} else {
+			setPwValid(false);
+		}
+	};
+
+	useEffect(() => {
+		if (emailValid && pwValid) {
+			setNotAllow(false);
+			return;
+		}
+		setNotAllow(true);
+	}, [emailValid, pwValid]);
+
 	return (
 		<Container className="page">
 			<TitleWrap className="titleWrap">
@@ -109,22 +144,33 @@ export default function Login() {
 				<ContentWrap>
 					<InputTitle>이메일 주소</InputTitle>
 					<InputWrap>
-						<Input placeholder="test@gmail.com" />
+						<Input type="text" placeholder="test@gmail.com" value={email} onChange={handleEmail} />
 					</InputWrap>
-					<ErrorMessageWrap>올바른 이메일을 입력해주세요</ErrorMessageWrap>
+					<ErrorMessageWrap>
+						{!emailValid && email.length > 0 && <div>올바른 이메일을 입력해주세요.</div>}
+					</ErrorMessageWrap>
 				</ContentWrap>
 
 				<ContentWrap>
 					<InputTitle>비밀번호</InputTitle>
 					<InputWrap>
-						<Input placeholder="영문, 숫자, 특수문자 포함 8자 이상" />
+						<Input
+							type="password"
+							placeholder="영문, 숫자, 특수문자 포함 8자 이상"
+							value={pw}
+							onChange={handlePassword}
+						/>
 					</InputWrap>
-					<ErrorMessageWrap>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</ErrorMessageWrap>
+					<ErrorMessageWrap>
+						{!pwValid && pw.length > 0 && (
+							<div>영문, 숫자, 특수문자 포함 8자 이상 입력해주세요.</div>
+						)}
+					</ErrorMessageWrap>
 				</ContentWrap>
 			</Content>
 
 			<div>
-				<Button disabled={true}>확 인</Button>
+				<Button disabled={notAllow}>확 인</Button>
 			</div>
 		</Container>
 	);
